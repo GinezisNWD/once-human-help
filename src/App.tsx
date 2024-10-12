5014d3b890fa397cd2ffb1d83549bf79425745c4
Ginezis <Ginezis25@gmail.com>
2024-10-12 21:46:15 +0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { HomePage } from './pages/HomePage'
+import { AboutPage } from './pages/AboutPage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
