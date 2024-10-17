b1c579c1ce174ea563912c3cba8b23a566eaf68a
Ginezis <Ginezis25@gmail.com>
2024-10-17 15:58:34 +0300

    add a temp solution for getAcs12CorrosionDps

--- src/utils/simulation.ts
+++ src/utils/simulation.ts
@@ -1,4 +1,4 @@
-import { IWeaponStats } from "../models/IStats";
+import { combinedStats, ISimulationReport, IWeaponStats } from "../models/IStats";
 
 export function isCrit(critRate: number) {
   return critRate >= Math.random() * 100
@@ -49,7 +49,13 @@ export function getDamagePerShot(stats: IWeaponStats, isCrit: boolean, isWeakspo
 
 export function getMps7Dps(stats: IWeaponStats) {
   const copyStats = structuredClone(stats)
-  const detailedDpsCheck = []
+
+  const result: ISimulationReport = {
+    totalDamage: 0,
+    weaponDamage: 0,
+    elementalDamage: null,
+    detailedDpsCheck: []
+  }
 
   for (let i = 0; i < copyStats.magazineCapacity; i++) {
     const shot = {
@@ -58,10 +64,42 @@ export function getMps7Dps(stats: IWeaponStats) {
       isWeakspot: isWeakspot(copyStats.weakspotHitRate)
     }
     shot.damage = getDamagePerShot(copyStats, shot.isCrit, shot.isWeakspot)
-    detailedDpsCheck.push(shot)
+    result.detailedDpsCheck.push(shot)
   }
 
-  const totalDamage = detailedDpsCheck.reduce((acc, elem) => acc + elem.damage, 0)
+  result.weaponDamage = result.detailedDpsCheck.reduce((acc, elem) => acc + elem.damage, 0)
 
-  return { totalDamage, detailedDpsCheck }
+  result.totalDamage = result.weaponDamage + (result.elementalDamage ?? 0)
+
+  return result
 }
+
+
+export function getAcs12CorrosionDps<T extends combinedStats>(stats: T) {
+  const copyStats = structuredClone(stats)
+
+  const result: ISimulationReport = {
+    totalDamage: 0,
+    weaponDamage: 0,
+    elementalDamage: null,
+    detailedDpsCheck: []
+  }
+
+  for (let i = 0; i < copyStats.magazineCapacity; i++) {
+    const shot = {
+      damage: 0,
+      isCrit: isCrit(copyStats.critRate),
+      isWeakspot: isWeakspot(copyStats.weakspotHitRate)
+    }
+    shot.damage = getDamagePerShot(copyStats, shot.isCrit, shot.isWeakspot)
+    result.detailedDpsCheck.push(shot)
+  }
+
+  result.weaponDamage = result.detailedDpsCheck.reduce((acc, elem) => acc + elem.damage, 0)
+
+  result.totalDamage = result.weaponDamage + (result.elementalDamage ?? 0)
+
+  return result
+}
+
+
